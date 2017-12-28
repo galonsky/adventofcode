e0b2aeb4d346f466dfbc76655a13562f47b6a5a2
Alex Galonsky <alex@galonsky.com>
2017-12-28 12:16:49 -0500

    day 3 part 2

--- day3.py
+++ day3.py
@@ -13,4 +13,73 @@ def spiral_distance(n):
     return ring + dist_from_axis
 
 
-print(spiral_distance(312051))
+def key(x, y):
+    return ','.join((str(x), str(y)))
+
+def set_with_sum_of_neighbors(spiral, x, y):
+    sum = 0
+    sum += spiral.get(key(x + 1, y), 0)
+    sum += spiral.get(key(x - 1, y), 0)
+    sum += spiral.get(key(x, y + 1), 0)
+    sum += spiral.get(key(x, y - 1), 0)
+    sum += spiral.get(key(x + 1, y + 1), 0)
+    sum += spiral.get(key(x + 1, y - 1), 0)
+    sum += spiral.get(key(x - 1, y + 1), 0)
+    sum += spiral.get(key(x - 1, y - 1), 0)
+
+    spiral[key(x, y)] = sum
+    return sum
+
+def iterate_spiral():
+    spiral = {}
+    spiral[key(0, 0)] = 1
+    spiral[key(1, 0)] = 1
+    x = 1
+    y = 0
+    cur = 3
+    side = 2
+    while True:
+
+
+        # last iteration has gone one extra right
+        # need to go up
+        for i in range(side - 1):
+            y += 1
+            val = set_with_sum_of_neighbors(spiral, x, y)
+            if val > 312051:
+                print(val)
+                return
+
+            # spiral[key(x, y)] = cur
+            # cur += 1
+        for i in range(side):
+            x -= 1
+            val = set_with_sum_of_neighbors(spiral, x, y)
+            if val > 312051:
+                print(val)
+                return
+            # spiral[key(x, y)] = cur
+            # cur += 1
+        for i in range(side):
+            y -= 1
+            val = set_with_sum_of_neighbors(spiral, x, y)
+            if val > 312051:
+                print(val)
+                return
+            # spiral[key(x, y)] = cur
+            # cur += 1
+        side += 2
+        for i in range(side - 1):
+            x += 1
+            val = set_with_sum_of_neighbors(spiral, x, y)
+            if val > 312051:
+                print(val)
+                return
+            # spiral[key(x, y)] = cur
+            # cur += 1
+
+
+
+
+iterate_spiral()
+#print(spiral_distance(312051))
