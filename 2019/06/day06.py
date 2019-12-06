2c7c9d6cbfc88596bc6c21b17fa8c81aef74f8b6
Alex Galonsky <alex.galonsky@carta.com>
2019-12-06 11:27:32 -0500

    part 2

--- 2019/06/day06.py
+++ 2019/06/day06.py
@@ -34,12 +34,40 @@ def get_sum_of_depths(root: Node, graph: Dict[str, Node], depth=0) -> int:
         total += get_sum_of_depths(graph[orbiting], graph, depth + 1)
     return total + depth
 
+def get_path(root: Node, graph: Dict[str, Node], target: str) -> List[str]:
+    if not root.orbited_by:
+        return None
+    if target in root.orbited_by:
+        return [target]
+    
+    for orbiting in root.orbited_by:
+        path = get_path(graph[orbiting], graph, target)
+        if path:
+            new_path = [orbiting]
+            new_path.extend(path)
+            return new_path
+
+
 def get_checksum(filename: str) -> int:
     graph = build_graph(filename)
     # print(graph)
     root = graph['COM']
     return get_sum_of_depths(root, graph)
 
+def get_shortest_path(filename: str):
+    graph = build_graph(filename)
+    # print(graph)
+    root = graph['COM']
+    you_path = get_path(root, graph, 'YOU')
+    san_path = get_path(root, graph, 'SAN')
+    i = 0
+    while i < max(len(you_path), len(san_path)) and you_path[i] == san_path[i]:
+        i += 1
+    
+    # i is now index after ancestor
+    return (len(you_path) - i) + (len(san_path) - i) - 2  # minus 2 since we want what YOU and SAN are orbiting
 
 
-print(get_checksum('input.txt'))
+
+
+print(get_shortest_path('input.txt'))
