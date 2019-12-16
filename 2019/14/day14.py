facdf22636b2cec75d65b33a62f9f2084bfe2798
Alex Galonsky <alex@galonsky.com>
2019-12-15 19:47:20 -0500

    part 2

--- 2019/14/day14.py
+++ 2019/14/day14.py
@@ -32,31 +32,6 @@ def get_recipes(filename):
             ingredients = [QuantifiedChemical.from_str(ingredient) for ingredient in left_parts]
             yield Recipe(ingredients=ingredients, result=right_chem)
 
-def get_num_needed(quantified_chem: QuantifiedChemical, resource_chem: str, recipes: Dict[str, Recipe]):
-
-    if quantified_chem.chemical == resource_chem:
-        return quantified_chem.quantity
-    
-    if quantified_chem.chemical == 'ORE':
-        return 0
-
-    recipe = recipes[quantified_chem.chemical]
-    ingredients = recipe.ingredients
-
-    # if inventory[quantified_chem.chemical] >= quantified_chem.quantity:
-    #     inventory[quantified_chem.chemical] -= quantified_chem.quantity
-    #     return 0
-    
-    # needed_qty = quantified_chem.quantity - inventory[quantified_chem.chemical]
-    # inventory[quantified_chem.chemical] = 0
-    
-    times_to_use_recipe = math.ceil(quantified_chem.quantity / recipe.result.quantity)
-    needed_sum = 0
-    for ingredient in ingredients:
-        needed_qty = ingredient.quantity * times_to_use_recipe
-        needed_sum += get_num_needed(QuantifiedChemical(chemical=ingredient.chemical, quantity=needed_qty), resource_chem, recipes)
-    return needed_sum
-
 def get_cost(quantified_chem: QuantifiedChemical, recipes: Dict[str, Recipe], inventory: Dict[str, int]):
 
     if quantified_chem.chemical == 'ORE':
@@ -93,11 +68,23 @@ def get_cost_of_one_fuel(filename):
     assert root.result.quantity == 1
 
     return get_cost(root.result, recipes_by_result_chem, defaultdict(lambda: 0))
-    # infinite_chems = [chem for chem in recipes_by_result_chem.keys() if recipes_by_result_chem[chem].ingredients[0].chemical == 'ORE']
 
-    # needed_map = {chem: get_num_needed(root.result, chem, recipes_by_result_chem) for chem in infinite_chems}
-    # return sum((get_cost(QuantifiedChemical(chemical=chem, quantity=qty), recipes_by_result_chem) for chem, qty in needed_map.items()))
+def get_num_fuel_possible(filename, ore=1000000000000):
+    recipes = get_recipes(filename)
+    recipes_by_result_chem = {recipe.result.chemical: recipe for recipe in recipes}
 
+    root = recipes_by_result_chem['FUEL']
+    assert root.result.quantity == 1
+    cost_of_one = get_cost(root.result, recipes_by_result_chem, defaultdict(lambda: 0))
+
+    inventory = defaultdict(lambda: 0)
+    total_fuel = 0
+    while cost_of_one <= ore:
+        fuel_to_aim_for = ore // cost_of_one
+        total_fuel += fuel_to_aim_for
+        cost = get_cost(QuantifiedChemical(chemical='FUEL', quantity=fuel_to_aim_for), recipes_by_result_chem, inventory)
+        ore -= cost
+    return total_fuel
 
 assert get_cost_of_one_fuel('sample1.txt') == 31
 assert get_cost_of_one_fuel('sample2.txt') == 165
@@ -105,3 +92,8 @@ assert get_cost_of_one_fuel('sample3.txt') == 13312
 assert get_cost_of_one_fuel('sample4.txt') == 180697
 assert get_cost_of_one_fuel('sample5.txt') == 2210736
 assert get_cost_of_one_fuel('input.txt') == 248794
+
+assert get_num_fuel_possible('sample3.txt') == 82892753
+assert get_num_fuel_possible('sample4.txt') == 5586022
+assert get_num_fuel_possible('sample5.txt') == 460664
+assert get_num_fuel_possible('input.txt') == 4906796
