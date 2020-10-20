7af0cbd73c7ce5674857b5c158da7c6f1297e91e
Alex Galonsky <alex@galonsky.com>
2020-10-20 12:27:57 -0400

    part 2!

--- 2019/18/day18.py
+++ 2019/18/day18.py
@@ -21,7 +21,7 @@ def get_dist_map(x: int, y: int, map: List[str]) -> Dict[str, Tuple[int, str]]:
     while queue:
         x, y, route, distance = queue.pop()
         ch = map[y][x]
-        if ch.isalpha() and distance > 0:
+        if ch not in ".@1234#" and distance > 0:
             dist_map[ch] = (distance, route)
             route += ch
         visited.add((x, y))
@@ -36,7 +36,7 @@ def precalc_distances(map: List[str]) -> Dict[str, Dict[str, Tuple[int, str]]]:
     distance_maps = {}
     for y, line in enumerate(map):
         for x, ch in enumerate(line):
-            if ch.islower() or ch == '@':
+            if ch.islower() or ch in '@1234':
                 dist_map = get_dist_map(x, y, map)
                 distance_maps[ch] = dist_map
     return distance_maps
@@ -81,6 +81,44 @@ def distance_to_get_keys(cur: str, distance_maps: Dict[str, Dict[str, Tuple[int,
     return result
 
 
+ROBOTS = ['1', '2', '3', '4']
+
+
+def distance_to_get_keys2(robot: str, rob_to_key: Dict[str, str], distance_maps: Dict[str, Dict[str, Tuple[int, str]]], keys: Set[str], cache: Dict[Tuple[str, int, int], int]) -> int:
+    if not keys:
+        return 0
+
+    cur = rob_to_key[robot]
+
+    cache_key = (''.join(sorted(list(keys))), cur, tuple(sorted(rob_to_key.items())))
+    if cache_key in cache:
+        return cache[cache_key]
+
+    result = math.inf
+    best_rob_to_key = None
+    current_keys = distance_maps.keys() - keys
+    for r in ROBOTS:
+        robloc = rob_to_key[r]
+        reachable_keys = {
+            k for k in keys
+            if distance_maps.get(robloc, {}).get(k) and all((c in current_keys or c.lower() in current_keys for c in list(distance_maps[robloc][k][1])))
+        }
+        for key in sorted(reachable_keys):
+            distance = distance_maps[robloc][key][0]
+            new_rob_to_key = dict(rob_to_key)
+            new_rob_to_key[r] = key
+            # print(f"Robot {r} took {key}")
+            new_distance = distance + distance_to_get_keys2(r, new_rob_to_key, distance_maps, keys - {key}, cache)
+            if new_distance < result:
+                result = new_distance
+                best_rob_to_key = new_rob_to_key
+
+
+    rob_to_key.update(best_rob_to_key)
+    cache[cache_key] = result
+    return result
+
+
 def get_all_keys(map: List[str]) -> Set[str]:
     all_keys = set()
     for line in map:
@@ -88,6 +126,37 @@ def get_all_keys(map: List[str]) -> Set[str]:
             if ch.islower():
                 all_keys.add(ch)
     return all_keys
+
+
+def find_char(map: List[str], char: str) -> Tuple[int, int]:
+    for y, line in enumerate(map):
+        for x, ch in enumerate(line):
+            if ch == char:
+                return x, y
+    raise Exception
+
+
+def edit_map(map: List[str], x, y, ch):
+    row = map[y]
+    chars = list(row)
+    chars[x] = ch
+    map[y] = ''.join(chars)
+
+
+
+def modify_map_for_part2(map: List[str]):
+    startx, starty = find_char(map, '@')
+    newmap = list(map)
+    edit_map(newmap, startx, starty, '#')
+    edit_map(newmap, startx+1, starty, '#')
+    edit_map(newmap, startx, starty+1, '#')
+    edit_map(newmap, startx-1, starty, '#')
+    edit_map(newmap, startx, starty-1, '#')
+    edit_map(newmap, startx + 1, starty+1, '1')
+    edit_map(newmap, startx+1, starty - 1, '2')
+    edit_map(newmap, startx - 1, starty-1, '3')
+    edit_map(newmap, startx-1, starty + 1, '4')
+    return newmap
 
 
 def run(filename: str):
@@ -98,11 +167,33 @@ def run(filename: str):
     return distance_to_get_keys("@", distance_maps, all_keys, {})
 
 
+def run_part_2(filename: str):
+    map = get_input(filename)
+    newmap = modify_map_for_part2(map)
+    all_keys = get_all_keys(map)
+    # for line in newmap:
+    #     print(line)
+    distance_maps = precalc_distances(newmap)
+    # print(distance_maps)
+    rob_to_key = {
+        r: r for r in ROBOTS
+    }
+    return distance_to_get_keys2("1", rob_to_key, distance_maps, all_keys, {})
+
+
 if __name__ == "__main__":
     # run('sample1.txt')
-    assert run('sample1.txt') == 8
-    assert run('sample2.txt') == 86
-    assert run('sample3.txt') == 132
-    assert run('sample4.txt') == 136
-    assert run('sample5.txt') == 81
-    assert run("input.txt") == 5262
+    # assert run('sample1.txt') == 8
+    # assert run('sample2.txt') == 86
+    # assert run('sample3.txt') == 132
+    # assert run('sample4.txt') == 136
+    # assert run('sample5.txt') == 81
+    # assert run("input.txt") == 5262
+
+    # print(run_part_2("sample9.txt"))
+
+    # assert run_part_2("sample6.txt") == 8
+    # assert run_part_2("sample7.txt") == 24
+    # assert run_part_2("sample8.txt") == 32
+    # assert run_part_2("sample9.txt") == 72
+    print(run_part_2("input.txt"))
