8d277d44433091056057496453af2ec220e582db
Alex Galonsky <alex.galonsky@carta.com>
2020-12-03 09:24:10 -0500

    day 3 part 2

--- 2020/03/day3.py
+++ 2020/03/day3.py
@@ -9,8 +9,7 @@ def get_slope(filename: str) -> List[str]:
     return lines
 
 
-if __name__ == '__main__':
-    slope = get_slope('input.txt')
+def get_trees_for_pattern(slope: List[str], y_diff: int, x_diff: int) -> int:
     width = len(slope[0])
     y = 0
     x = 0
@@ -18,7 +17,20 @@ if __name__ == '__main__':
     while y < len(slope):
         if slope[y][x % width] == '#':
             num_trees += 1
-        y += 1
-        x += 3
+        y += y_diff
+        x += x_diff
+    return num_trees
 
-    print(num_trees)
+
+if __name__ == '__main__':
+    slope = get_slope('input.txt')
+
+    # print(get_trees_for_pattern(slope, 1, 3))
+
+    print(
+        get_trees_for_pattern(slope, 1, 1)
+        * get_trees_for_pattern(slope, 1, 3)
+        * get_trees_for_pattern(slope, 1, 5)
+        * get_trees_for_pattern(slope, 1, 7)
+        * get_trees_for_pattern(slope, 2, 1)
+    )
