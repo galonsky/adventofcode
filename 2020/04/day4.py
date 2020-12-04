2c5a2f5503e3b31d2bdb113733c825717a3b0372
Alex Galonsky <alex.galonsky@carta.com>
2020-12-04 11:04:19 -0500

    day 4 part 2

--- 2020/04/day4.py
+++ 2020/04/day4.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 
@@ -16,6 +17,53 @@ def get_passports(filename: str) -> List[dict]:
         return pass_dicts
 
 
+def validate_birth_year(val: str) -> bool:
+    return len(val) == 4 and 1920 <= int(val) <= 2002
+
+
+def validate_issue_year(val: str) -> bool:
+    return len(val) == 4 and 2010 <= int(val) <= 2020
+
+
+def validate_expiration_year(val: str) -> bool:
+    return len(val) == 4 and 2020 <= int(val) <= 2030
+
+
+def validate_height(val: str) -> bool:
+    unit = val[-2:]
+    if unit == 'cm':
+        return 150 <= int(val[:-2]) <= 193
+    else:
+        return 59 <= int(val[:-2]) <= 76
+
+
+def validate_hair_color(val: str) -> bool:
+    pattern = re.compile(r'^#[0-9a-f]{6}$')
+    return bool(pattern.match(val))
+
+
+def validate_eye_color(val: str) -> bool:
+    return val in (
+        'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth',
+    )
+
+
+def validate_passport_id(val: str) -> bool:
+    pattern = re.compile(r'^[0-9]{9}$')
+    return bool(pattern.match(val))
+
+
+VALIDATORS = {
+    'byr': validate_birth_year,
+    'iyr': validate_issue_year,
+    'eyr': validate_expiration_year,
+    'hgt': validate_height,
+    'hcl': validate_hair_color,
+    'ecl': validate_eye_color,
+    'pid': validate_passport_id,
+}
+
+
 if __name__ == '__main__':
     passports = get_passports('input.txt')
     required_keys = {
@@ -30,5 +78,6 @@ if __name__ == '__main__':
     count = 0
     for passport in passports:
         if required_keys <= passport.keys():
-            count += 1
+            if all(validator(passport[key]) for (key, validator) in VALIDATORS.items()):
+                count += 1
     print(count)
