794d2d80049ea8a3d9a1a3c6b700b140dc6633df
Alex Galonsky <alex@galonsky.com>
2020-12-06 10:33:17 -0500

    day 6 part 2

--- 2020/06/day6.py
+++ 2020/06/day6.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Iterable, List
 
 
@@ -16,5 +17,18 @@ def part1():
     print(total_count)
 
 
+def part2():
+    total_count = 0
+    for group in get_raw_groups('input.txt'):
+        people = group.split('\n')
+        counts = defaultdict(int)
+        for person in people:
+            for ch in person:
+                counts[ch] += 1
+
+        total_count += len([key for key in counts if counts[key] == len(people)])
+    print(total_count)
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
