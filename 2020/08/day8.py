810808d65545c617920633ad73f7e9cdea1217f5
Alex Galonsky <alex@galonsky.com>
2020-12-08 20:20:53 -0500

    day 8 part 2

--- 2020/08/day8.py
+++ 2020/08/day8.py
@@ -33,7 +33,44 @@ def part1():
             raise Exception(f'unrecognized instruction! {inst}')
 
 
+class InfiniteLoopException(Exception):
+    pass
+
+
+def run_program(instructions: List[Tuple[str, int]]) -> int:
+    executed = set()
+    pc = 0
+    acc = 0
+    while True:
+        if pc in executed:
+            raise InfiniteLoopException
+        if pc == len(instructions):
+            return acc
+        executed.add(pc)
+        inst, val = instructions[pc]
+        if inst == 'nop':
+            pc += 1
+        elif inst == 'acc':
+            acc += val
+            pc += 1
+        elif inst == 'jmp':
+            pc += val
+        else:
+            raise Exception(f'unrecognized instruction! {inst}')
+
+def part2():
+    instructions = get_instructions('input.txt')
+    for i, (inst, val) in enumerate(instructions):
+        if inst == 'acc':
+            continue
+        new_inst = 'nop' if inst == 'jmp' else 'jmp'
+        new_instructions = list(instructions)
+        new_instructions[i] = (new_inst, val)
+        try:
+            return run_program(new_instructions)
+        except InfiniteLoopException:
+            pass
 
 
 if __name__ == '__main__':
-    print(part1())
+    print(part2())
