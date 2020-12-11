7306f29743cebadc1cfbc866acc82d258c42dda6
Alex Galonsky <alex.galonsky@carta.com>
2020-12-10 20:32:58 -0500

    day 10 part 2

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Set, Dict
 
 
 def get_adapters(filename: str) -> Iterable[int]:
@@ -28,5 +28,31 @@ def part1():
     print(one_diffs * three_diffs)
 
 
+def get_number_of_ways(start_jolt: int, end_jolt: int, adapters: Set[int], cache: Dict[int, int]) -> int:
+    if start_jolt == end_jolt:
+        return 1
+
+    if start_jolt in cache:
+        return cache[start_jolt]
+
+    total = 0
+    if start_jolt + 1 in adapters:
+        total += get_number_of_ways(start_jolt + 1, end_jolt, adapters, cache)
+    if start_jolt + 2 in adapters:
+        total += get_number_of_ways(start_jolt + 2, end_jolt, adapters, cache)
+    if start_jolt + 3 in adapters:
+        total += get_number_of_ways(start_jolt + 3, end_jolt, adapters, cache)
+    cache[start_jolt] = total
+    return total
+
+
+def part2():
+    adapters = set(get_adapters('input.txt'))
+    end_jolt = max(adapters) + 3
+    adapters.add(end_jolt)
+    cache = {}
+    print(get_number_of_ways(0, end_jolt, adapters, cache))
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
