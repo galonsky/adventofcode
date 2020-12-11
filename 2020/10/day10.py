25379e2f90112d70c22649ad1723b87074fa6654
Alex Galonsky <alex.galonsky@carta.com>
2020-12-10 20:18:26 -0500

    day 10 part 1

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -8,4 +8,25 @@ def get_adapters(filename: str) -> Iterable[int]:
 
 
 def part1():
-    
+    adapters = list(get_adapters('input.txt'))
+    adapters.append(max(adapters) + 3)
+    adapters.sort()
+    last = 0
+
+    one_diffs = 0
+    three_diffs = 0
+    for adapter in adapters:
+        diff = adapter - last
+        if diff == 1:
+            one_diffs += 1
+        elif diff == 3:
+            three_diffs += 1
+        else:
+            raise Exception('unexpected!')
+        last = adapter
+
+    print(one_diffs * three_diffs)
+
+
+if __name__ == '__main__':
+    part1()
