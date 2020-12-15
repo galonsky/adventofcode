08e8c759b2cb8792ff6fb9cb273c91bbc39e9260
Alex Galonsky <alex.galonsky@carta.com>
2020-12-14 20:55:50 -0500

    day 14 part 2

--- 2020/14/day14.py
+++ 2020/14/day14.py
@@ -17,6 +17,23 @@ def mask_val(mask: str, val: int) -> int:
     return (val & zeroes) | ones
 
 
+def recursive_replace_floating_vals(mask: str, address: int) -> Iterable[int]:
+    index = mask.find('X')
+    if index == -1:
+        yield address
+        return
+
+    new_mask = mask.replace('X', '0', 1)  # doesn't matter what, just need to get rid of X
+    one_mask = 1 << (35 - index)
+    yield from recursive_replace_floating_vals(new_mask, address | one_mask)
+    yield from recursive_replace_floating_vals(new_mask, address & ~one_mask)
+
+
+def mask_address(mask: str, address: int) -> Iterable[int]:
+    masked_address = address | int(mask.replace('X', '0'), 2)
+    yield from recursive_replace_floating_vals(mask, masked_address)
+
+
 def part1():
     mask_str = ''
     mem = {}
@@ -34,5 +51,24 @@ def part1():
     ])
 
 
+def part2():
+    mask_str = ''
+    mem = {}
+    lines = get_lines('input.txt')
+    for line in lines:
+        if line.startswith('mask'):
+            mask_str = line[7:]
+        else:
+            match = MEM_PATTERN.match(line)
+            loc = int(match.group(1))
+            val = int(match.group(2))
+            addresses = mask_address(mask_str, loc)
+            for address in addresses:
+                mem[address] = val
+    return sum([
+        val for val in mem.values() if val != 0
+    ])
+
+
 if __name__ == '__main__':
-    print(part1())
+    print(part2())
