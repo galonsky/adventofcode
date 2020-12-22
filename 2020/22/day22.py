76db55a556def4da86718117a769cb8b4af1939b
Alex Galonsky <alex.galonsky@carta.com>
2020-12-22 11:36:41 -0500

    day 22 part 2

--- 2020/22/day22.py
+++ 2020/22/day22.py
@@ -1,5 +1,6 @@
 from collections import deque
-from typing import Iterable, Deque
+from itertools import islice
+from typing import Iterable, Deque, List, Callable
 
 
 def get_decks(filename: str) -> Iterable[Deque[int]]:
@@ -10,22 +11,67 @@ def get_decks(filename: str) -> Iterable[Deque[int]]:
             yield deque(map(int, player.split('\n')[1:]))
 
 
-def part1():
-    decks = list(get_decks('input.txt'))
+def get_history_key(decks: List[Deque[int]]) -> str:
+    return ','.join(map(str, decks[0])) + '|' + ','.join(map(str, decks[0]))
+
+
+def play_game(decks: List[Deque[int]]) -> int:
     while all(len(deck) > 0 for deck in decks):
         player1_card = decks[0].popleft()
         player2_card = decks[1].popleft()
-
         if player1_card > player2_card:
             decks[0].append(player1_card)
             decks[0].append(player2_card)
         elif player2_card > player1_card:
             decks[1].append(player2_card)
             decks[1].append(player1_card)
-        else:
-            raise Exception('undefined?')
+    return 0 if len(decks[1]) == 0 else 1
 
-    winner_deck = decks[0] if len(decks[1]) == 0 else decks[1]
+
+def play_recursive_game(decks: List[Deque[int]]) -> int:
+    round_history = set()
+    while all(len(deck) > 0 for deck in decks):
+        history_key = get_history_key(decks)
+        if history_key in round_history:
+            return 0
+        round_history.add(history_key)
+
+        player1_card = decks[0].popleft()
+        player2_card = decks[1].popleft()
+        if len(decks[0]) >= player1_card and len(decks[1]) >= player2_card:
+            player1_copy = deque(islice(decks[0], player1_card))
+            player2_copy = deque(islice(decks[1], player2_card))
+            winner = play_recursive_game([player1_copy, player2_copy])
+        else:
+            winner = 0 if player1_card > player2_card else 1
+
+        if winner == 0:
+            decks[0].append(player1_card)
+            decks[0].append(player2_card)
+        else:
+            decks[1].append(player2_card)
+            decks[1].append(player1_card)
+    return 0 if len(decks[1]) == 0 else 1
+
+
+def part1():
+    decks = list(get_decks('input.txt'))
+
+    winner_index = play_game(decks)
+    winner_deck = decks[winner_index]
+    score = 0
+    multiplier = 1
+    for card in reversed(winner_deck):
+        score += card * multiplier
+        multiplier += 1
+    return score
+
+
+def part2():
+    decks = list(get_decks('input.txt'))
+
+    winner_index = play_recursive_game(decks)
+    winner_deck = decks[winner_index]
     score = 0
     multiplier = 1
     for card in reversed(winner_deck):
@@ -36,4 +82,4 @@ def part1():
 
 
 if __name__ == '__main__':
-    print(part1())
+    print(part2())
