1e1670309d95511e7ec3bda4b66b8f0c19656cec
Alex Galonsky <alex.galonsky@carta.com>
2020-12-24 10:11:24 -0500

    day 24 part 1

---
+++ 2020/24/day24.py
@@ -0,0 +1,50 @@
+from typing import Iterable, Dict, Tuple
+
+
+POSSIBLE_DIRECTIONS = {
+    'e': (1.0, 0.0),
+    'se': (0.5, -1.0),
+    'sw': (-0.5, -1.0),
+    'w': (-1.0, 0.0),
+    'nw': (-0.5, 1.0),
+    'ne': (0.5, 1.0),
+}
+
+
+def get_tile_directions(filename: str) -> Iterable[str]:
+    with open(filename, 'r') as file:
+        for line in file:
+            yield line.rstrip('\n')
+
+
+def get_next_direction(dir_string: str) -> str:
+    for direction in POSSIBLE_DIRECTIONS:
+        if dir_string.startswith(direction):
+            return direction
+
+
+def get_direction_steps(directions: str) -> Iterable[str]:
+    i = 0
+    while i < len(directions):
+        next_dir = get_next_direction(directions[i:])
+        yield next_dir
+        i += len(next_dir)
+
+
+
+def part1():
+    directions = get_tile_directions('input.txt')
+    tile_colors: Dict[Tuple[float, float], bool] = {}  # missing/false means white, true means black
+    for direction in directions:
+        x, y = 0.0, 0.0
+        for step in get_direction_steps(direction):
+            dx, dy = POSSIBLE_DIRECTIONS[step]
+            x += dx
+            y += dy
+        tile_colors[(x, y)] = not tile_colors.get((x, y), False)
+
+    return len([val for val in tile_colors.values() if val])
+
+
+if __name__ == '__main__':
+    print(part1())
