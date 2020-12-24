310088f09809564b284ab3ea06535a392df2f93d
Alex Galonsky <alex.galonsky@carta.com>
2020-12-24 13:43:09 -0500

    day 24 part 2

--- 2020/24/day24.py
+++ 2020/24/day24.py
@@ -1,5 +1,4 @@
-from typing import Iterable, Dict, Tuple
-
+from typing import Iterable, Dict, Tuple, Set
 
 POSSIBLE_DIRECTIONS = {
     'e': (1.0, 0.0),
@@ -38,6 +37,46 @@ def part1():
     return len([val for val in tile_colors.values() if val])
 
 
+def get_neighborhood(coords: Iterable[Tuple[float, float]]) -> Set[Tuple[float, float]]:
+    neighborhood = set()
+    for coord in coords:
+        x, y = coord
+        neighborhood.add(coord)
+        for dx, dy in POSSIBLE_DIRECTIONS.values():
+            neighborhood.add((x + dx, y + dy))
+    return neighborhood
+
+
+def flip_tiles(tile_colors: Dict[Tuple[float, float], bool]) -> Dict[Tuple[float, float], bool]:
+    new_tile_colors = dict(tile_colors)
+    for coord in get_neighborhood(tile_colors.keys()):
+        black = tile_colors.get(coord, False)
+        white = not black
+
+        coord_neighborhood = get_neighborhood([coord])
+        num_black_neighbors = len(
+            [neighbor for neighbor in (coord_neighborhood - {coord}) if tile_colors.get(neighbor, False)]
+        )
+
+        if black and (
+            num_black_neighbors == 0
+            or num_black_neighbors > 2
+        ):
+            del new_tile_colors[coord]
+        elif white and num_black_neighbors == 2:
+            new_tile_colors[coord] = True
+    return new_tile_colors
+
+
+def part2():
+    directions = get_tile_directions('input.txt')
+    tile_colors = get_tile_colors(directions)
+    for _ in range(100):
+        tile_colors = flip_tiles(tile_colors)
+
+    return len([val for val in tile_colors.values() if val])
+
+
 def get_tile_colors(directions: Iterable[str]) -> Dict[Tuple[float, float], bool]:
     tile_colors: Dict[Tuple[float, float], bool] = {}  # missing/false means white, true means black
     for direction in directions:
@@ -51,4 +90,4 @@ def get_tile_colors(directions: Iterable[str]) -> Dict[Tuple[float, float], bool
 
 
 if __name__ == '__main__':
-    print(part1())
+    print(part2())
