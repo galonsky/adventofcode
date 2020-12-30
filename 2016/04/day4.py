b67306e4697b04a497fc996843796b179824bd20
Alex Galonsky <alex@galonsky.com>
2020-12-30 11:06:59 -0500

    day 4 part 1

---
+++ 2016/04/day4.py
@@ -0,0 +1,33 @@
+import re
+from collections import Counter
+from typing import Iterable
+
+PATTERN = re.compile(r'((?:[a-z]+-)+)(\d+)\[([a-z]+)]')
+
+
+def get_rooms(filename: str) -> Iterable[str]:
+    with open(filename, 'r') as file:
+        for line in file:
+            yield line.rstrip('\n')
+
+
+def part1():
+    real_rooms = 0
+    rooms = get_rooms('input.txt')
+    for room in rooms:
+        match = PATTERN.match(room)
+        encrypted, room_id, checksum = match.groups()
+        counter = Counter()
+        for ch in encrypted:
+            if ch == '-':
+                continue
+            counter[ch] += 1
+        most_common_then_alphabetical = sorted(counter.most_common(), key=lambda tu: (-tu[1], tu[0]))[:5]
+        most_common_set = {t[0] for t in most_common_then_alphabetical}
+        if most_common_set == set(checksum):
+            real_rooms += int(room_id)
+    return real_rooms
+
+
+if __name__ == '__main__':
+    print(part1())
