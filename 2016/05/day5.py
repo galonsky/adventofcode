c192b7f7315eeec87bad07229114598c8b872b27
Alex Galonsky <alex@galonsky.com>
2020-12-31 10:10:31 -0500

    day 5 part 2

--- 2016/05/day5.py
+++ 2016/05/day5.py
@@ -1,4 +1,5 @@
 import hashlib
+from typing import List, Optional
 
 
 def hash(value: str) -> str:
@@ -19,5 +20,22 @@ def find_password(door_id: str) -> str:
     return password
 
 
+def find_password_v2(door_id: str) -> str:
+    password: List[Optional[str]] = [None] * 8
+    i = 0
+    while any((v is None for v in password)):
+        value = door_id + str(i)
+        hashed = hash(value)
+        if hashed.startswith('00000'):
+            index = int(hashed[5], 16)
+            char = hashed[6]
+            if index < 0 or index >= 8 or password[index] is not None:
+                pass
+            else:
+                password[index] = char
+        i += 1
+    return ''.join(password)
+
+
 if __name__ == '__main__':
-    print(find_password('uqwqemis'))
+    print(find_password_v2('uqwqemis'))
