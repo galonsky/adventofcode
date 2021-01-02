38419086d26cea3e1f335f602839babb875dec7e
Alex Galonsky <alex@galonsky.com>
2021-01-02 11:21:24 -0500

    day 7 part 2

--- 2016/07/day7.py
+++ 2016/07/day7.py
@@ -1,5 +1,6 @@
 import re
-from typing import Iterable
+from functools import reduce
+from typing import Iterable, Set
 
 BRACKET_PATTERN = re.compile(r'\[([a-z]+)]')
 
@@ -15,6 +16,18 @@ def has_abba(string: str) -> bool:
     return False
 
 
+def get_babs(string: str) -> Set[str]:
+    babs = set()
+    if len(string) < 3:
+        return babs
+    for i in range(len(string) - 2):
+        if string[i] == string[i+1]:
+            continue
+        if string[i] == string[i+2]:
+            babs.add(string[i:i+3])
+    return babs
+
+
 def supports_tls(string: str) -> bool:
     bracket_matches = BRACKET_PATTERN.finditer(string)
     for match in bracket_matches:
@@ -23,6 +36,21 @@ def supports_tls(string: str) -> bool:
     non_bracket_strings = BRACKET_PATTERN.split(string)[0::2]  # skipping every other since it's including capturing group
     for string in non_bracket_strings:
         if has_abba(string):
+            return True
+    return False
+
+
+def get_opposite(string: str) -> str:
+    return string[1] + string[0] + string[1]
+
+
+def supports_ssl(string: str) -> bool:
+    bracket_matches = BRACKET_PATTERN.finditer(string)
+    all_babs = reduce(lambda s1, s2: s1 | s2, [get_babs(match.group(1)) for match in bracket_matches])
+    non_bracket_strings = BRACKET_PATTERN.split(string)[0::2]  # skipping every other since it's including capturing group
+    all_abas = reduce(lambda s1, s2: s1 | s2, [get_babs(string) for string in non_bracket_strings])
+    for aba in all_abas:
+        if get_opposite(aba) in all_babs:
             return True
     return False
 
@@ -45,5 +73,18 @@ def part1():
     print(num_tls)
 
 
+def part2():
+    # print(supports_ssl('aba[bab]xyz'))
+    # print(supports_ssl('xyx[xyx]xyx'))
+    # print(supports_ssl('aaa[kek]eke'))
+    # print(supports_ssl('zazbz[bzb]cdb'))
+
+    num_ssl = 0
+    for string in get_input('input.txt'):
+        if supports_ssl(string):
+            num_ssl += 1
+    print(num_ssl)
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
