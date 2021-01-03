76cf3e280ac30cefcc654b0d33f0e458e69f2509
Alex Galonsky <alex@galonsky.com>
2021-01-03 11:34:22 -0500

    day 8 part 1

---
+++ 2016/08/day8.py
@@ -0,0 +1,58 @@
+import re
+from typing import List, Iterable
+
+RECT_PATTERN = re.compile(r'rect (\d+)x(\d+)')
+ROW_PATTERN = re.compile(r'rotate row y=(\d+) by (\d+)')
+COL_PATTERN = re.compile(r'rotate column x=(\d+) by (\d+)')
+
+
+def get_input(filename: str) -> Iterable[str]:
+    with open(filename, 'r') as file:
+        for line in file:
+            yield line.rstrip('\n')
+
+
+def rect(screen: List[List[bool]], x: int, y: int):
+    for _y in range(y):
+        for _x in range(x):
+            screen[_y][_x] = True
+
+
+def rotate_row(screen: List[List[bool]], y: int, b: int):
+    screen[y] = screen[y][-b:] + screen[y][:-b]
+
+
+def rotate_column(screen: List[List[bool]], x: int, b: int):
+    original_col = [row[x] for row in screen]
+    new_col = original_col[-b:] + original_col[:-b]
+    for y in range(len(screen)):
+        screen[y][x] = new_col[y]
+
+
+def part1():
+    screen = [
+        [False] * 50 for _ in range(6)
+    ]
+    for instruction in get_input('input.txt'):
+        if match := RECT_PATTERN.match(instruction):
+            x, y = match.groups()
+            rect(screen, int(x), int(y))
+        elif match := ROW_PATTERN.match(instruction):
+            y, b = match.groups()
+            rotate_row(screen, int(y), int(b))
+        elif match := COL_PATTERN.match(instruction):
+            x, b = match.groups()
+            rotate_column(screen, int(x), int(b))
+        else:
+            raise Exception('unexpected!')
+
+    num_on = 0
+    for row in screen:
+        for light in row:
+            if light:
+                num_on += 1
+    print(num_on)
+
+
+if __name__ == '__main__':
+    part1()
