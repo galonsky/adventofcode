8d5fdc642d66b27cbcfa23145f40013008e73df8
Alex Galonsky <alex@galonsky.com>
2021-01-04 18:35:04 -0500

    day 9 part 2

--- 2016/09/day9.py
+++ 2016/09/day9.py
@@ -5,9 +5,6 @@ MARKER_PATTERN = re.compile(r'\((\d+)x(\d+)\)')
 
 
 def decompress(compressed: str) -> str:
-    match = MARKER_PATTERN.search(compressed)
-    if not match:
-        return compressed
     result = ''
     remaining = compressed
     while True:
@@ -27,6 +24,21 @@ def decompress(compressed: str) -> str:
             remaining = remaining[match.end()+pattern_len:]
 
 
+def get_decompressed_length(compressed: str) -> int:
+    match = MARKER_PATTERN.search(compressed)
+    if not match:
+        return len(compressed)
+
+    pattern_len = int(match.group(1))
+    repetitions = int(match.group(2))
+    pattern = compressed[match.end():match.end() + pattern_len]
+
+    return (
+        match.start()  # everything before the marker
+        + repetitions * get_decompressed_length(pattern)
+        + get_decompressed_length(compressed[match.end()+pattern_len:])
+    )
+
 def part1():
     # print(decompress('ADVENT'))
     # print(decompress('A(1x5)BC'))
@@ -38,5 +50,14 @@ def part1():
     print(len(decompressed))
 
 
+def part2():
+    # print(get_decompressed_length('(3x3)XYZ'))
+    # print(get_decompressed_length('X(8x2)(3x3)ABCY'))
+    # print(get_decompressed_length('(27x12)(20x12)(13x14)(7x10)(1x12)A'))
+    # print(get_decompressed_length('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'))
+    print(get_decompressed_length(open('input.txt').read()))
+
+
+
 if __name__ == '__main__':
-    part1()
+    part2()
