f755bc6c2a1a82c002e9a60956d7b4869e572904
Alex Galonsky <alex.galonsky@carta.com>
2021-12-01 09:30:52 -0500

    part 2

--- 2021/01/day1.py
+++ 2021/01/day1.py
@@ -17,5 +17,18 @@ def get_number_increases(measurements: Iterable[int]) -> int:
     return increases
 
 
+def get_window_increases(measurements: Iterable[int]) -> int:
+    measurements = list(measurements)
+    last = None
+    increases = 0
+    for i in range(len(measurements) - 3 + 1):
+        window = sum(measurements[i:i+3]) if last is None else (last - measurements[i-1] + measurements[i+2])
+        if last is not None and window > last:
+            increases += 1
+        last = window
+    return increases
+
+
 if __name__ == '__main__':
     print(get_number_increases(get_measurements('input.txt')))
+    print(get_window_increases(get_measurements('input.txt')))
