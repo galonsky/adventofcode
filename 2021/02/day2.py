f17fc1d5c6806cc3b87cb8b8f879d2f25eb9f341
Alex Galonsky <alex.galonsky@carta.com>
2021-12-02 09:32:29 -0500

    part 2

--- 2021/02/day2.py
+++ 2021/02/day2.py
@@ -30,7 +30,26 @@ def get_final_position(commands: Iterable[Command]) -> tuple[int, int]:
     return horiz, depth
 
 
+def get_final_position_aim(commands: Iterable[Command]) -> tuple[int, int]:
+    horiz = 0
+    depth = 0
+    aim = 0
+    for command in commands:
+        if command.direction == "forward":
+            horiz += command.magnitude
+            depth += aim * command.magnitude
+        elif command.direction == "up":
+            aim -= command.magnitude
+        elif command.direction == "down":
+            aim += command.magnitude
+        else:
+            raise Exception('unexpected!')
+    return horiz, depth
+
+
 if __name__ == '__main__':
-    commands = get_commands("input.txt")
+    commands = list(get_commands("input.txt"))
     horiz, depth = get_final_position(commands)
+    print(horiz * depth)
+    horiz, depth = get_final_position_aim(commands)
     print(horiz * depth)
