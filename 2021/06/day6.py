b26f679471f9d037a12297201c55dc2ea79d0683
Alex Galonsky <alex.galonsky@carta.com>
2021-12-06 17:19:39 -0500

    day 6 part 1

---
+++ 2021/06/day6.py
@@ -0,0 +1,27 @@
+from collections import deque
+from typing import Iterable
+
+
+INPUT = [
+1,1,1,1,1,5,1,1,1,5,1,1,3,1,5,1,4,1,5,1,2,5,1,1,1,1,3,1,4,5,1,1,2,1,1,1,2,4,3,2,1,1,2,1,5,4,4,1,4,1,1,1,4,1,3,1,1,1,2,1,1,1,1,1,1,1,5,4,4,2,4,5,2,1,5,3,1,3,3,1,1,5,4,1,1,3,5,1,1,1,4,4,2,4,1,1,4,1,1,2,1,1,1,2,1,5,2,5,1,1,1,4,1,2,1,1,1,2,2,1,3,1,4,4,1,1,3,1,4,1,1,1,2,5,5,1,4,1,4,4,1,4,1,2,4,1,1,4,1,3,4,4,1,1,5,3,1,1,5,1,3,4,2,1,3,1,3,1,1,1,1,1,1,1,1,1,4,5,1,1,1,1,3,1,1,5,1,1,4,1,1,3,1,1,5,2,1,4,4,1,4,1,2,1,1,1,1,2,1,4,1,1,2,5,1,4,4,1,1,1,4,1,1,1,5,3,1,4,1,4,1,1,3,5,3,5,5,5,1,5,1,1,1,1,1,1,1,1,2,3,3,3,3,4,2,1,1,4,5,3,1,1,5,5,1,1,2,1,4,1,3,5,1,1,1,5,2,2,1,4,2,1,1,4,1,3,1,1,1,3,1,5,1,5,1,1,4,1,2,1
+]
+
+
+def get_num_fish(timers: Iterable[int], num_days: int) -> int:
+    current_fish = deque(timers)
+
+    for _ in range(num_days):
+        new_fish = deque()
+        while current_fish:
+            timer = current_fish.popleft()
+            if timer == 0:
+                new_fish.append(6)
+                new_fish.append(8)
+            else:
+                new_fish.append(timer - 1)
+        current_fish = new_fish
+    return len(current_fish)
+
+
+if __name__ == '__main__':
+    print(get_num_fish(INPUT, 80))
