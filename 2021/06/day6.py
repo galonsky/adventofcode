952ef400b6f191ace9c23a8ce5b4de7ab847c3e4
Alex Galonsky <alex.galonsky@carta.com>
2021-12-06 18:11:54 -0500

    part 2 wip

--- 2021/06/day6.py
+++ 2021/06/day6.py
@@ -23,5 +23,72 @@ def get_num_fish(timers: Iterable[int], num_days: int) -> int:
     return len(current_fish)
 
 
+def get_num_children(starting_timer: int, num_days: int) -> int:
+    """
+    3
+    2
+    1
+    0
+    6   6
+    5   5
+    4   4
+    3   3
+    2   2
+    1   1
+    0   0
+    6   6   6   6
+
+    double every day = 2^x
+    double ever 6 days = 2 ^ (x / 6)
+
+
+
+
+    3
+    2
+    1
+    0
+    6   8
+    5   7
+    4   6
+    3   5
+    2   4
+    1   3
+    0   2
+    6   1   8
+    5   0   7
+    4   6   6   8
+    3   5   5   7
+    2   4   4   6
+    1   3   3   5
+    0   2   2   4
+    6   1   1   3   8
+    5   0   0   2   7
+    4   6   6   1   6   8
+    3   5   5   0   5   7
+    2   4   4   6   4   6   8
+    1   3   3   5   3   5   7
+    0   2   2   4   2   4   6
+    6   1   1   3   1   3   5   8
+    5   0   0   2   0   2   4   7
+    4   6   6   1   6   1   3   6   8   8   8
+    3   5   5   0   5   0   2   5   7   7   7
+    2   4   4   6   4   6   1   4   6   6   6   8   8
+    1   3   3   5   3   5   0   3   5   5   5   7   7
+    0   2   2   4   2   4   6   2   4   4   4   6   6   8
+
+
+    N = 3
+    0: 0
+    1: 0
+    2: 0
+    3: 0
+    4: 1
+
+    fish of timer N will produce ~ N // 7 direct descendents
+
+    """
+
+
 if __name__ == '__main__':
-    print(get_num_fish(INPUT, 80))
+    print(get_num_fish([3], 256))
