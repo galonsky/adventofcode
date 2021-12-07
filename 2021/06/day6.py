ed508433ea90175d80f575f17b4ba918452a5caf
Alex Galonsky <alex@galonsky.com>
2021-12-06 20:22:47 -0500

    part 2

--- 2021/06/day6.py
+++ 2021/06/day6.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import defaultdict
 from typing import Iterable
 
 
@@ -7,88 +7,22 @@ INPUT = [
 ]
 
 
-def get_num_fish(timers: Iterable[int], num_days: int) -> int:
-    current_fish = deque(timers)
+def get_num_fish_fast(timers: Iterable[int], num_days: int) -> int:
+    current_fish = defaultdict(int)
+    for timer in timers:
+        current_fish[timer] += 1
 
     for _ in range(num_days):
-        new_fish = deque()
-        while current_fish:
-            timer = current_fish.popleft()
+        new_fish = defaultdict(int)
+        for timer, count in current_fish.items():
             if timer == 0:
-                new_fish.append(6)
-                new_fish.append(8)
+                new_fish[6] += count
+                new_fish[8] += count
             else:
-                new_fish.append(timer - 1)
+                new_fish[timer - 1] += count
         current_fish = new_fish
-    return len(current_fish)
-
-
-def get_num_children(starting_timer: int, num_days: int) -> int:
-    """
-    3
-    2
-    1
-    0
-    6   6
-    5   5
-    4   4
-    3   3
-    2   2
-    1   1
-    0   0
-    6   6   6   6
-
-    double every day = 2^x
-    double ever 6 days = 2 ^ (x / 6)
-
-
-
-
-    3
-    2
-    1
-    0
-    6   8
-    5   7
-    4   6
-    3   5
-    2   4
-    1   3
-    0   2
-    6   1   8
-    5   0   7
-    4   6   6   8
-    3   5   5   7
-    2   4   4   6
-    1   3   3   5
-    0   2   2   4
-    6   1   1   3   8
-    5   0   0   2   7
-    4   6   6   1   6   8
-    3   5   5   0   5   7
-    2   4   4   6   4   6   8
-    1   3   3   5   3   5   7
-    0   2   2   4   2   4   6
-    6   1   1   3   1   3   5   8
-    5   0   0   2   0   2   4   7
-    4   6   6   1   6   1   3   6   8   8   8
-    3   5   5   0   5   0   2   5   7   7   7
-    2   4   4   6   4   6   1   4   6   6   6   8   8
-    1   3   3   5   3   5   0   3   5   5   5   7   7
-    0   2   2   4   2   4   6   2   4   4   4   6   6   8
-
-
-    N = 3
-    0: 0
-    1: 0
-    2: 0
-    3: 0
-    4: 1
-
-    fish of timer N will produce ~ N // 7 direct descendents
-
-    """
+    return sum(count for count in current_fish.values())
 
 
 if __name__ == '__main__':
-    print(get_num_fish([3], 256))
+    print(get_num_fish_fast(INPUT, 256))
