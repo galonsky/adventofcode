dcdcff75f049b0949d5ec6ec6f50e976138fa38e
Alex Galonsky <alex@galonsky.com>
2021-12-09 20:13:50 -0500

    part 2

--- 2021/09/day9.py
+++ 2021/09/day9.py
@@ -34,5 +34,33 @@ def get_sum_of_risk(heights: list[str]) -> int:
     return risk
 
 
+def floodfill_size(heights: list[str], x: int, y: int) -> int:
+    if x < 0 or y < 0 or y >= len(heights) or x >= len(heights[0]):
+        return 0
+
+    if heights[y][x] in {"9", "x"}:
+        return 0
+
+    line_arr = list(heights[y])
+    line_arr[x] = "x"
+    heights[y] = "".join(line_arr)
+
+    neighbor_sizes = sum(
+        floodfill_size(heights, x + v[0], y + v[1]) for v in VECTORS
+    )
+    return 1 + neighbor_sizes
+
+
+def get_basin_score(heights: list[str]) -> int:
+    basins = []
+    for y, line in enumerate(heights):
+        for x, ch in enumerate(line):
+            if heights[y][x] not in {"9", "x"}:
+                basins.append(floodfill_size(heights, x, y))
+    basins.sort(reverse=True)
+    return basins[0] * basins[1] * basins[2]
+
+
 if __name__ == '__main__':
-    print(get_sum_of_risk(get_map("input.txt")))
+    # print(get_sum_of_risk(get_map("input.txt")))
+    print(get_basin_score(get_map("input.txt")))
