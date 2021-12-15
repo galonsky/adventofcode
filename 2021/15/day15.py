cb4de59c8c04d89c1ca9ff334d38d1abf69f9526
Alex Galonsky <alex.galonsky@carta.com>
2021-12-15 11:44:08 -0500

    part 2

--- 2021/15/day15.py
+++ 2021/15/day15.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from queue import PriorityQueue
 from typing import Iterable
 
@@ -57,7 +56,76 @@ def find_lowest_total_risk(cavern_map: list[list[int]]) -> int:
     return dist[(dest_x, dest_y)]
 
 
+def get_5_by_5_risk(cavern_map: list[list[int]], x: int, y: int) -> int:
+    x_region = x // len(cavern_map)
+    y_region = y // len(cavern_map)
+    region_addend = x_region + y_region
+
+    original_x = x % len(cavern_map)
+    original_y = y % len(cavern_map)
+
+    new_risk = (cavern_map[original_y][original_x] + region_addend)
+    if new_risk > 9:
+        return new_risk % 9
+    return new_risk
+
+
+def print_5_by_5(cavern_map: list[list[int]]):
+    for y in range(5 * len(cavern_map)):
+        line = ""
+        for x in range(5 * len(cavern_map)):
+            line += str(get_5_by_5_risk(cavern_map, x, y))
+        print(line)
+
+
+def find_lowest_total_risk_5_by_5(cavern_map: list[list[int]]) -> int:
+    dest_y = 5 * len(cavern_map) - 1
+    dest_x = 5 * len(cavern_map) - 1
+
+    dist = {}
+    prev = {}
+
+    q = PriorityQueue()
+
+    for y in range(5 * len(cavern_map)):
+        for x in range(5 * len(cavern_map)):
+            dist[(x, y)] = float("inf")
+            q.put((0, (x, y)))
+
+    dist[(0, 0)] = 0
+    while q:
+        p, u = q.get()
+        if p != dist[u]:
+            continue
+
+        def neighbors() -> Iterable[tuple[int, int]]:
+            for vec in (
+                (0, 1),
+                (1, 0),
+                (-1, 0),
+                (0, -1),
+            ):
+                newx = u[0] + vec[0]
+                newy = u[1] + vec[1]
+                if 0 <= newx <= dest_x and 0 <= newy <= dest_y:
+                    yield newx, newy
+
+
+        if u == (dest_x, dest_y):
+            return dist[(dest_x, dest_y)]
+
+        for v in neighbors():
+            alt = dist[u] + get_5_by_5_risk(cavern_map, v[0], v[1])
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+                q.put((alt, v))
+
+    return dist[(dest_x, dest_y)]
+
+
 if __name__ == '__main__':
     cavern_map = get_map("input.txt")
+    print_5_by_5(cavern_map)
     # print(cavern_map)
-    print(find_lowest_total_risk(cavern_map))
+    print(find_lowest_total_risk_5_by_5(cavern_map))
