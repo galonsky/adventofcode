031b3880ea41471f0cfcc00c1634a1637245016d
Alex Galonsky <alex.galonsky@carta.com>
2021-12-16 17:38:29 -0500

    parsing headers and literals

--- 2021/16/day16.py
+++ 2021/16/day16.py
@@ -1,4 +1,7 @@
-from typing import Iterator
+from _operator import add
+from functools import reduce
+from itertools import islice
+from typing import Iterator, Iterable
 
 
 class HexBitwiseIterator(Iterator):
@@ -14,7 +17,7 @@ class HexBitwiseIterator(Iterator):
         else:
             self.bit_index += 1
 
-        if self.hex_index == len(self.hex_string):
+        if self.hex_index >= len(self.hex_string):
             raise StopIteration
 
         return (int(self.hex_string[self.hex_index], 16) >> (3 - self.bit_index)) & 1
@@ -24,7 +27,42 @@ class HexBitwiseIterator(Iterator):
     def __iter__(self) -> Iterator[int]:
         return self
 
+    def finish_hex_digit(self) -> None:
+        self.hex_index += 1
+        self.bit_index = 0
+
+
+def bits_to_int(bits: Iterable[int]) -> int:
+    return int(reduce(
+        add, map(str, bits)
+    ), 2)
+
+
+def get_literal(bits: HexBitwiseIterator) -> int:
+    bit_string = ""
+    while True:
+        prefix = next(bits)
+        bit_string += "".join(map(str, islice(bits, 4)))
+        if prefix == 0:
+            bits.finish_hex_digit()
+            return int(bit_string, 2)
+
+
+
+def get_sum_of_versions(hex_string: str) -> int:
+    bit_iter = HexBitwiseIterator(hex_string)
+    while True:
+        version = bits_to_int(islice(bit_iter, 3))
+        type_id = bits_to_int(islice(bit_iter, 3))
+        print(version)
+        print(type_id)
+        if type_id == 4:
+            literal = get_literal(bit_iter)
+            print(literal)
+        else:
+            raise NotImplementedError
+        return
+
 
 if __name__ == '__main__':
-    iterator = HexBitwiseIterator("AB")
-    print(list(iterator))
+    get_sum_of_versions("D2FE28")
