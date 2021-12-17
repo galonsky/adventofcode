46e95a290b62076db096365aa6963ca9ef587b35
Alex Galonsky <alex.galonsky@carta.com>
2021-12-17 14:10:50 -0500

    17 part 1

--- 2021/17/day17.py
+++ 2021/17/day17.py
@@ -1,3 +1,4 @@
+from math import sqrt, ceil
 from typing import Optional
 
 
@@ -23,9 +24,35 @@ def does_probe_hit(vx: int, vy: int, target_x: tuple[int, int], target_y: tuple[
             max_y = y
 
 
+def get_starting_velocity(distance: int) -> int:
+    return ceil((-1 + sqrt(1 + 8*distance)) / 2)
+
+
+def find_max_y(target_x: tuple[int, int], target_y: tuple[int, int]) -> int:
+    vx_min = get_starting_velocity(target_x[0])
+    vx_max = get_starting_velocity(target_x[1])
+
+    max_y = 0
+    for vx in range(vx_min, vx_max + 1):
+        for vy in range(-1000, 1000):
+            y = does_probe_hit(vx, vy, target_x, target_y)
+            if y is not None:
+                # print(f"{vx} {vy} Reached {y}")
+                max_y = max(y, max_y)
+
+    return max_y
+
+
+
 if __name__ == '__main__':
-    assert does_probe_hit(7, 2, (20, 30), (-10, -5)) is not None
-    assert does_probe_hit(6, 3, (20, 30), (-10, -5)) is not None
-    assert does_probe_hit(9, 0, (20, 30), (-10, -5)) is not None
-    assert does_probe_hit(17, -4, (20, 30), (-10, -5)) is None
-    assert does_probe_hit(6, 9, (20, 30), (-10, -5)) == 45
+    # assert does_probe_hit(7, 2, (20, 30), (-10, -5)) is not None
+    # assert does_probe_hit(6, 3, (20, 30), (-10, -5)) is not None
+    # assert does_probe_hit(9, 0, (20, 30), (-10, -5)) is not None
+    # assert does_probe_hit(17, -4, (20, 30), (-10, -5)) is None
+    # assert does_probe_hit(6, 9, (20, 30), (-10, -5)) == 45
+
+    # print(does_probe_hit(21, 53, (230, 283), (-107, -57)))
+
+    # print(find_max_y((20, 30), (-10, -5)))
+    print(find_max_y((230, 283), (-107, -57)))
+    # 1378 too low
