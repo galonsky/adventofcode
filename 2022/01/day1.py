a680ed8e4d84b682b003516e7c06c3fda452b31d
Alex Galonsky <alex@galonsky.com>
2022-12-01 09:41:47 -0500

    day 1 part 1

---
+++ 2022/01/day1.py
@@ -0,0 +1,23 @@
+from typing import Generator
+
+
+def iterate_elves(filename: str) -> Generator[list[int], None, None]:
+    with open(filename, "r") as file:
+        current_elf = []
+        for line in file:
+            if not line.strip():
+                yield current_elf
+                current_elf = []
+            else:
+                current_elf.append(int(line.strip()))
+        if current_elf:
+            yield current_elf
+
+
+def find_highest_calorie_elf(filename: str) -> int:
+    elves = iterate_elves(filename)
+    return max((sum(c_list) for c_list in elves))
+
+
+if __name__ == '__main__':
+    print(find_highest_calorie_elf("input.txt"))
