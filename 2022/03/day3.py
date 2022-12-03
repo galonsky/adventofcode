57cc416c821f25392ae79e51863c04bcf4a343b6
Alex Galonsky <alex@galonsky.com>
2022-12-03 11:48:03 -0500

    part 2

--- 2022/03/day3.py
+++ 2022/03/day3.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import Generator
 
 
@@ -15,6 +16,10 @@ def get_priority(rucksack: str) -> int:
     firsthalf = rucksack[:len(rucksack) // 2]
     secondhalf = rucksack[len(rucksack) // 2:]
     shared_char = _get_shared_char(firsthalf, secondhalf)
+    return get_char_priority(shared_char)
+
+
+def get_char_priority(shared_char):
     if shared_char.islower():
         return ord(shared_char) - ord('a') + 1
     else:
@@ -26,5 +31,18 @@ def get_sum_of_priorities(filename: str) -> int:
     return sum(get_priority(sack) for sack in rucksacks)
 
 
+def get_sum_of_group_priorities(filename: str) -> int:
+    rucksack_iter = iter(get_rucksacks(filename))
+    priorities = 0
+    try:
+        while True:
+            groups = [set(next(rucksack_iter)) for _ in range(3)]
+            char_set = reduce(lambda acc, sack: acc.intersection(sack), groups)
+            priorities += get_char_priority(next(iter(char_set)))
+    except StopIteration:
+        pass
+    return priorities
+
+
 if __name__ == '__main__':
-    print(get_sum_of_priorities("input.txt"))
+    print(get_sum_of_group_priorities("input.txt"))
