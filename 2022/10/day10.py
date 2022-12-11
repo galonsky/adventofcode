7e0f701c2fcdfeda9540aecc2e9bd9a9fab2f352
Alex Galonsky <alex@galonsky.com>
2022-12-11 18:49:18 -0500

    part 2

--- 2022/10/day10.py
+++ 2022/10/day10.py
@@ -7,6 +7,16 @@ def get_instructions(filename: str) -> Generator[str, None, None]:
             yield line.strip()
 
 
+def print_crt(reg_by_cycle: dict[int, int]) -> None:
+    for row in range(6):
+        for col in range(40):
+            cycle_no = row * 40 + col + 1
+            sprite_middle = reg_by_cycle[cycle_no]
+            ch = '#' if col in (sprite_middle - 1, sprite_middle, sprite_middle + 1) else ' '
+            print(ch, end="")
+        print()
+
+
 def get_signal_strength(instructions: Iterable[str]) -> int:
     reg_by_cycle = {
         1: 1,
@@ -24,7 +34,12 @@ def get_signal_strength(instructions: Iterable[str]) -> int:
             reg_by_cycle[cycle_count + 2] = reg_by_cycle[cycle_count] + to_add
             cycle_count += 2
 
+    print_crt(reg_by_cycle)
+
     cycles_to_test = [20, 60, 100, 140, 180, 220]
+
+
+
     return sum(cyc * reg_by_cycle[cyc] for cyc in cycles_to_test)
 
 
