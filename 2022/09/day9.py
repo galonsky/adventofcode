f298908d008c4d708d6ae4b52653d0c27c775de1
Alex Galonsky <alex@galonsky.com>
2022-12-11 11:23:40 -0500

    refactor

--- 2022/09/day9.py
+++ 2022/09/day9.py
@@ -20,52 +20,8 @@ def are_touching(head: tuple[int, int], tail: tuple[int, int]) -> bool:
     return abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1
 
 
-def get_num_locations_tail_visited(instructions: Iterable[Tuple[str, int]]) -> int:
-    head = (0, 0)
-    tail = head
-
-    unique_tail_locations = set()
-    unique_tail_locations.add(tail)
-
-    for instruction in instructions:
-        vector = VECTORS_BY_DIRECTION[instruction[0]]
-        for _ in range(instruction[1]):
-            head = (head[0] + vector[0], head[1] + vector[1])
-
-            if are_touching(head, tail):
-                continue
-
-            if tail == (head[0] + 2, head[1]):
-                # tail is to the right
-                tail = (tail[0] - 1, tail[1])
-            elif tail == (head[0] - 2, head[1]):
-                # tail is to the left
-                tail = (tail[0] + 1, tail[1])
-            elif tail == (head[0], head[1] + 2):
-                # tail is above
-                tail = (tail[0], tail[1] - 1)
-            elif tail == (head[0], head[1] - 2):
-                # tail is below
-                tail = (tail[0], tail[1] + 1)
-            elif head[0] - tail[0] > 0 and head[1] - tail[1] > 0:
-                # head is northeast
-                tail = (tail[0] + 1, tail[1] + 1)
-            elif head[0] - tail[0] < 0 and head[1] - tail[1] > 0:
-                # head is northwest
-                tail = (tail[0] - 1, tail[1] + 1)
-            elif head[0] - tail[0] > 0 and head[1] - tail[1] < 0:
-                # head is southeast
-                tail = (tail[0] + 1, tail[1] - 1)
-            elif head[0] - tail[0] < 0 and head[1] - tail[1] < 0:
-                # head is southwest
-                tail = (tail[0] - 1, tail[1] - 1)
-
-            unique_tail_locations.add(tail)
-    return len(unique_tail_locations)
-
-
-def get_num_locations_tail_visited_ten_knots(instructions: Iterable[Tuple[str, int]]) -> int:
-    knots = [(0, 0) for _ in range(10)]
+def get_num_locations_tail_visited(instructions: Iterable[Tuple[str, int]], num_knots: int) -> int:
+    knots = [(0, 0) for _ in range(num_knots)]
 
     unique_tail_locations = set()
     unique_tail_locations.add(knots[-1])
@@ -110,4 +66,6 @@ def get_num_locations_tail_visited_ten_knots(instructions: Iterable[Tuple[str, i
 
 if __name__ == '__main__':
     instructions = get_instructions("input.txt")
-    print(get_num_locations_tail_visited_ten_knots(instructions))
+    print(get_num_locations_tail_visited(instructions, 2))
+    instructions = get_instructions("input.txt")
+    print(get_num_locations_tail_visited(instructions, 10))
