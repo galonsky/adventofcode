24db079ab9761f37758a7d45dbbf1da9cc1c20e9
Alex Galonsky <alex.galonsky@carta.com>
2024-12-06 09:53:34 -0500

    part 2

--- 2024/03/day3.py
+++ 2024/03/day3.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass
 from typing import Iterable
 
 
+DO_PATTERN = re.compile(r'do\(\)')
+DONT_PATTERN = re.compile(r"don't\(\)")
 MUL_PATTERN = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+EXPR_PATTERN = re.compile(r"(do\(\))|(don't\(\))|(mul\(\d{1,3},\d{1,3}\))")
 
 
 @dataclass
@@ -25,11 +28,26 @@ def get_valid_instructions(memory: str) -> Iterable[Mul]:
         yield Mul(int(match.group(1)), int(match.group(2)))
 
 
+def get_valid_instructions_do_dont(memory: str) -> Iterable[Mul]:
+    enabled = True
+    for match in EXPR_PATTERN.finditer(memory):
+        if match.lastindex == 1:
+            enabled = True
+        elif match.lastindex == 2:
+            enabled = False
+        elif match.lastindex == 3:
+            if enabled:
+                mul_match = MUL_PATTERN.match(match.group(3))
+                yield Mul(int(mul_match.group(1)), int(mul_match.group(2)))
+        else:
+            raise Exception('huh?')
+
+
 def get_sum_of_instructions(instructions: Iterable[Mul]) -> int:
     return sum(m.result() for m in instructions)
 
 
 if __name__ == '__main__':
     memory = get_memory("input.txt")
-    instructions = get_valid_instructions(memory)
+    instructions = get_valid_instructions_do_dont(memory)
     print(get_sum_of_instructions(instructions))
