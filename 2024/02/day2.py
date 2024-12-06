76b068cd6ed2262961499942d9f25b18ab7ff42a
Alex Galonsky <alex@galonsky.com>
2024-12-05 22:18:08 -0500

    part 2

--- 2024/02/day2.py
+++ 2024/02/day2.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Generator, Iterable
 
 
@@ -16,11 +17,21 @@ def is_report_safe(report: list[int]) -> bool:
         for i in range(len(differences))
     )
 
+def is_report_safe_with_dampener(report: list[int]) -> bool:
+    report_versions = [report[0:i] + report[i+1:] for i in range(len(report))]
+    report_versions.append(report)
+    return any(is_report_safe(r) for r in report_versions)
+
 
 def get_num_safe_reports(reports: Iterable[list[int]]) -> int:
     return len([report for report in reports if is_report_safe(report)])
 
 
+def get_num_safe_reports_dampener(reports: Iterable[list[int]]) -> int:
+    return len([report for report in reports if is_report_safe_with_dampener(report)])
+
+
 if __name__ == '__main__':
     reports = get_reports("input.txt")
-    print(get_num_safe_reports(reports))
+    # print(get_num_safe_reports(reports))
+    print(get_num_safe_reports_dampener(reports))
