f862c6db2f942d41c5d65d9b331d7b7791b1321f
Alex Galonsky <alex@galonsky.com>
2024-12-07 14:51:24 -0500

    part 2

--- 2024/04/day4.py
+++ 2024/04/day4.py
@@ -1,55 +1,24 @@
 from typing import Generator
 
-from pandas import DataFrame
-import numpy as np
-
-
-XMAS = ['X', 'M', 'A', 'S']
-SAMX = list(reversed(XMAS))
-
 
 def get_word_search(filename: str) -> list[str]:
     with open(filename, 'r') as file:
         return [line.strip() for line in file]
 
 
-def get_all_4_by_4(ws: list[str]) -> Generator[DataFrame, None, None]:
+def get_all_3_by_3(ws: list[str]) -> Generator[list[str], None, None]:
     height = len(ws)
     width = len(ws[0])
-    for y in range(height - 3):
-        for x in range(width - 3):
-            yield DataFrame([list(ws[y+i][x:x+4]) for i in range(4)])
-
-
-def get_all_rows(df: DataFrame) -> Generator[list[str], None, None]:
-    for _, row in df.iterrows():
-        yield row.tolist()
-    for _, row in df.T.iterrows():
-        yield row.tolist()
-    yield np.diagonal(df.to_numpy()).tolist()
-    yield np.fliplr(df.to_numpy()).diagonal().tolist()
-
-
-def row_matches(row: list[str]) -> int:
-    return 1 if row == XMAS or row == SAMX else 0
-
-
-def find_num_matches(ws: list[str]) -> int:
-    subsets = get_all_4_by_4(ws)
-    total = 0
-    for subset in subsets:
-        print(subset)
-        matches = sum(row_matches(row) for row in get_all_rows(subset))
-        print("num matches: {}".format(str(matches)))
-        total += matches
-    return total
+    for y in range(height - 2):
+        for x in range(width - 2):
+            yield [ws[y+i][x:x+3] for i in range(3)]
 
 
 def diag(ws: list[str], startx: int, starty: int, dx: int, dy: int) -> str:
     res = ""
     x = startx
     y = starty
-    while x < len(ws[0]) and y < len(ws) and x >= 0 and y >= 0:
+    while len(ws[0]) > x >= 0 and len(ws) > y >= 0:
         res += ws[y][x]
         x += dx
         y += dy
@@ -86,8 +55,19 @@ def get_all_xmas(ws: list[str]) -> int:
     return total
 
 
+def subset_has_xmas(subset: list[str]) -> bool:
+    diagonal = diag(subset, 0, 0, 1, 1)
+    antidiagonal = diag(subset, len(subset[0]) - 1, 0, -1, 1)
+
+    return diagonal in ("MAS", "SAM") and antidiagonal in ("MAS", "SAM")
+
+
+def get_all_xmas_new(ws: list[str]) -> int:
+    subsets = get_all_3_by_3(ws)
+    return len([ss for ss in subsets if subset_has_xmas(ss)])
+
 
 if __name__ == '__main__':
     ws = get_word_search("input.txt")
     # ws = ["abcd", "efgh", "ijkl", "mnop"]
-    print(get_all_xmas(ws))
+    print(get_all_xmas_new(ws))
