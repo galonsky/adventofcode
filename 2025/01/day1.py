3aa8fe975db1f1c048fbdb3f6fa4f0746a6585dc
Alex Galonsky <alex@galonsky.com>
2025-12-02 19:32:26 +0000

    day 1

--- 2025/01/day1.py
+++ 2025/01/day1.py
@@ -19,7 +19,32 @@ def get_password(rotations: Iterable[str]) -> int:
     return num_zeroes
 
 
+def get_password_2(rotations: Iterable[str]) -> int:
+    current = 50
+    num_zeroes = 0
+    for rotation in rotations:
+        direction = rotation[0]
+        magnitude = int(rotation[1:])
+        num_zeroes += magnitude // 100
+        if direction == 'L':
+            newval = (current - magnitude) % 100
+            if current != 0:
+                if newval == 0 or newval > current:
+                    print("mid added")
+                    num_zeroes += 1
+        else:
+            newval = (current + magnitude) % 100
+            if current != 0:
+                if newval == 0 or newval < current:
+                    print("mid added")
+                    num_zeroes += 1
+        current = newval
+        print(current)
+    return num_zeroes
+
+
 if __name__ == "__main__":
-    print(get_password(get_rotations("input.txt")))
+    # 6706 too high
+    print(get_password_2(get_rotations("input.txt")))
 
 
