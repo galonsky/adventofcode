f9b37326e3f4e9678784f49a6a938a1b48ca661f
Alex Galonsky <galonsky@users.noreply.github.com>
2025-12-04 04:22:57 +0000

    part 1

--- 2025/02/day2.py
+++ 2025/02/day2.py
@@ -1,6 +1,16 @@
 from math import log10
 
 
+def sum_ranges_in_file(filename: str) -> int:
+    with open(filename, 'r') as file:
+        content = file.read()
+        total = 0
+        for rng in content.split(","):
+            parts = rng.split("-")
+            total += sum_invalids_in_range(int(parts[0]), int(parts[1]))
+        return total
+
+
 def sum_invalids_in_range(start: int, end: int) -> int:
     cur = start
     target = 0
@@ -34,6 +44,12 @@ def sum_invalids_in_range(start: int, end: int) -> int:
 
 
 if __name__ == "__main__":
-    assert sum_invalids_in_range(11, 22) == 33
-    assert sum_invalids_in_range(95, 115) == 99
-    assert sum_invalids_in_range(998, 1012) == 1010
+    # assert sum_invalids_in_range(11, 22) == 33
+    # assert sum_invalids_in_range(95, 115) == 99
+    # assert sum_invalids_in_range(998, 1012) == 1010
+    # assert sum_invalids_in_range(1188511880, 1188511890) == 1188511885
+    # assert sum_invalids_in_range(222220, 222224) == 222222
+    # assert sum_invalids_in_range(1698522,1698528) == 0
+    # assert sum_invalids_in_range(446443, 446449) == 446446
+    # assert sum_invalids_in_range(38593856, 38593862) == 38593859
+    print(sum_ranges_in_file("input.txt"))
