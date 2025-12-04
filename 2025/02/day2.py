5ae318c77e4b33ed19a6ca669353f4635005a919
Alex Galonsky <galonsky@users.noreply.github.com>
2025-12-04 04:13:04 +0000

    more stuff

--- 2025/02/day2.py
+++ 2025/02/day2.py
@@ -1,13 +1,5 @@
 from math import log10
 
-def is_invalid(num: int) -> bool:
-    as_string = str(num)
-    length = len(as_string)
-    if length < 2 or length % 2 != 0:
-        return False
-
-    return as_string[:length] == as_string[length]
-
 
 def sum_invalids_in_range(start: int, end: int) -> int:
     cur = start
@@ -16,21 +8,32 @@ def sum_invalids_in_range(start: int, end: int) -> int:
     while cur <= end:
         print(f"cur: {cur}")
         num_digits = int(log10(cur)) + 1
+        # print(f"num digits: {num_digits}")
         if num_digits % 2 != 0:
-            cur = 10 ** (num_digits + 1)
-            if cur > end:
-                return total
+            cur = 10 ** (num_digits)
+            print(f"new cur: {cur}")
+            continue
 
         first_half = cur // (10 ** (num_digits // 2))
+        target = first_half * (10 ** (num_digits // 2)) + first_half
+
+        print(f"first half: {first_half}")
+        print(f"new target: {target}")
+
         if cur == target:
             total += cur
             # find next first half
-        else:
-            target = first_half * (10 ** (num_digits // 2)) + first_half
-            
+        
         if target <= cur:
             next_first_half = first_half + 1
+            target = next_first_half * (10 ** (num_digits // 2)) + next_first_half
             
-            target = next_first_half * (10 ** (num_digits // 2))
+
         cur = target
     return total
+
+
+if __name__ == "__main__":
+    assert sum_invalids_in_range(11, 22) == 33
+    assert sum_invalids_in_range(95, 115) == 99
+    assert sum_invalids_in_range(998, 1012) == 1010
