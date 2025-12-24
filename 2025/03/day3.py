cf0d786b55ae719b1d5b30984c180e113724cda0
Alex Galonsky <alex@galonsky.com>
2025-12-23 22:44:01 -0500

    part 2

--- 2025/03/day3.py
+++ 2025/03/day3.py
@@ -24,7 +24,31 @@ def get_joltage(bank: str) -> int:
 
     return highest * 10 + second_highest
 
+def get_max_and_index(corpus: list[int], start: int, end: int) -> tuple[int, int]:
+    highest = 0
+    index = 0
+    for i in range(start, end):
+        n = corpus[i]
+        if n > highest:
+            highest = n
+            index = i
+    return highest, index
+
+
+def get_joltage_12(bank: str) -> int:
+    num_found = 0
+    joltage_str = ""
+    last_chosen_index = -1
+    while num_found < 12:
+        # search space is after the last chosen but leaving enough room for 12 - num_found - 1 more
+        corpus = [int(ch) for ch in bank]
+        highest, last_chosen_index = get_max_and_index(corpus, last_chosen_index+1, len(bank) -(12-num_found-1))
+        joltage_str += str(highest)
+        num_found += 1
+    # print(joltage_str)
+    return int(joltage_str)
+
 
 if __name__ == '__main__':
-    print(sum(get_joltage(bank) for bank in get_banks("input.txt")))
+    print(sum(get_joltage_12(bank) for bank in get_banks("input.txt")))
 
